36e0513753ecc415bcaae5584bd09efcb992f235
patsys <pat.weber91@googlemail.com>
2018-06-01 23:36:01 +0200

    Update flaskDemo.py

--- flaskDemo.py
+++ flaskDemo.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/index.html' ,methods = ['GET', 'POST'])
 def hello():
     #    return "{\"key\":\"testtest\", \"policy\":{\"Plugins\":false,\"Export\":false,\"ExportNoKey\":false,\"Import\":false,\"Print\":false,\"PrintNoKey\":false,\"NewFile\":true,\"SaveFile\":true,\"AutoType\":true,\"AutoTypeWithoutContext\":false,\"CopyToClipboard\":false,\"CopyWholeEntries\":false,\"DragDrop\":false,\"UnhidePasswords\":false,\"ChangeMasterKey\":false,\"ChangeMasterKeyNoKey\":false,\"EditTriggers\":false}}"
-        return '{"key":"printf(\"%s\",\"TestTest\");"}'
+        return '{"key":"TestTest"}'
 
     if __name__ == '__main__':
             app.run(host='0.0.0.0', port=6080)
